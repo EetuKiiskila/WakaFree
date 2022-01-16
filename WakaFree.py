50ddc24cafa41f53dea278f0960f64748f920e12
Eetu Kiiskilä <eetu.kiiskila@gmail.com>
2022-01-16 15:26:36 +0200

    Add comments to class Stats

--- WakaFree.py
+++ WakaFree.py
@@ -11,6 +11,13 @@ import Plotting
 
 @dataclass
 class Stats:
+    """Data class that can store statistics read from a WakaTime JSON file.
+
+    :ivar type_: The type of the object. Possible values: "languages", "editors", "operating_systems".
+    :ivar daily_stats: Container for daily stats. Should be initialized as an empty dict.
+    :ivar keys: Container for labels. Should be initialized as an empty list.
+    :ivar total_times: Container for total times. Should be initialized as an empty list.
+    """
     type_: str
     daily_stats: dict
     keys: list

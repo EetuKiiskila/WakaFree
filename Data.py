485616a5bb34043af6870b21133de1e5dd8c6c74
Eetu Kiiskilä <eetu.kiiskila@gmail.com>
2022-05-16 11:16:19 +0300

    Clean code

--- Data.py
+++ Data.py
@@ -182,13 +182,16 @@ def read_stats(file_path: str) -> None:
             # Fetch keys for stats
             fetch_keys(date)
 
-        # Read stats and group under Other if wanted
+        # Read, group and sort data
         if "l" in (Args.graphs + Args.totals):
             populate_stats(stats, languages_stats)
             unify_stats(languages_stats, Args.minimum_labeling_percentage)
+            sort_stats(languages_stats)
         if "e" in (Args.graphs + Args.totals):
             populate_stats(stats, editors_stats)
             unify_stats(editors_stats, Args.minimum_labeling_percentage)
+            sort_stats(editors_stats)
         if "o" in (Args.graphs + Args.totals):
             populate_stats(stats, operating_systems_stats)
             unify_stats(operating_systems_stats, Args.minimum_labeling_percentage)
+            sort_stats(operating_systems_stats)
